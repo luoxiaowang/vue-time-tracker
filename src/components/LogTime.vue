f0c7144fa436a680d9b1262fac38c24a86458c4a
luoxiao <m15927510358@163.com>
2016-07-04 10:35:56 +0800

    finish

---
+++ src/components/LogTime.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="form-horizontal">
+    <div class="form-group">
+      <div class="col-sm-6">
+        <label>日期</label>
+        <input
+          type="date"
+          class="form-control"
+          v-model="timeEntry.date"
+          placeholder="Date"
+        />
+      </div>
+      <div class="col-sm-6">
+        <label>时间</label>
+        <input
+          type="number"
+          class="form-control"
+          v-model="timeEntry.totalTime"
+          placeholder="Hours"
+        />
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-sm-12">
+        <label>备注</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="timeEntry.comment"
+          placeholder="Comment"
+        />
+      </div>
+    </div>
+    <button class="btn btn-primary" @click="save()">保存</button>
+    <button v-link="'/time-entries'" class="btn btn-danger">取消</button>
+    <hr>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        //模拟一个默认值
+        timeEntry: {
+
+        }
+      }
+    },
+    methods: {
+      save () {
+          this.$http.post('http://localhost:8888/create',{
+            comment : this.timeEntry.comment,
+            totalTime : this.timeEntry.totalTime,
+            date : this.timeEntry.date
+          }).then(function(ret) {
+            console.log(ret);
+            let timeEntry = this.timeEntry
+            console.log(timeEntry);
+            this.$dispatch('timeUpdate', timeEntry)
+            this.timeEntry = {}
+          })
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
+import 'bootstrap/dist/css/bootstrap.css' 
+
 import Vue from 'vue'
 import App from './App'
+import Home from './components/Home.vue'
+import TimeEntries from './components/TimeEntries.vue'
+import LogTime from './components/LogTime.vue'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+import VueRouter from "vue-router"
+import VueResource from 'vue-resource'
+
+//注册两个插件
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter()
+
+// 路由map
+router.map({
+  '/Home': {
+    component: Home
+  },
+  '/time-entries': {
+    component: TimeEntries,
+    subRoutes: {
+      '/log-time': {
+        component: LogTime
+      }
+    }
+  }
 })
+
+router.redirect({
+  '*': '/Home'
+})
+
+
+router.start(App, '#app')
+
